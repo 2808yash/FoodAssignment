4822b769b80f8512ea9a131a75d3be6ceb5a7f00
Yash Sharma <67325907+2808yash@users.noreply.github.com>
2021-05-16 10:22:31 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>FoodShala</title>
+	<title>OrderNow</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
@@ -22,7 +22,7 @@
 <body>
 
 	<!-- navbar -->
-	<nav class="navbar  navbar-inverse navbar-fixed-top" style="font-size: 16px;">
+	<nav class="navbar navbar-inverse  navbar-fixed-top" style="font-size: 16px;background-color:rgba(230, 126, 34);border:0 solid white;">
 	  <div class="container">
 	    <div class="navbar-header">
 	    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -30,13 +30,13 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span> 
 		    </button>
-	      <a class="navbar-brand active" href="index.php" style="font-size: 18px;">FoodSHALA</a>
+	      <a class="navbar-brand active" href="index.php" style="font-size: 18px;color:white">OrderNOW</a>
 	    </div>
 	    <div class="collapse navbar-collapse" id="myNavbar">
 	      <ul class="nav navbar-nav navbar-right">
-	      	<li><a href="restaurant-signup.php">Join Us Today</a></li>
-	        <li><a href="user-signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-	        <li><a href="user-login.php"><span class="glyphicon glyphicon-log-in"></span> Sign in</a></li>
+	      	<li><a href="restaurant-signup.php" style="color:white">Join Us Today</a></li>
+	        <li><a href="user-signup.php" style="color:white"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+	        <li><a href="user-login.php" style="color:white"><span class="glyphicon glyphicon-log-in"></span> Sign in</a></li>
 	      </ul>
 	    </div>
 	  </div>
@@ -60,12 +60,12 @@
             		    <div class="card" style="width:24rem;">
 						    <img class="card-img-top" src="<?php echo $row['item_imagepath']; ?>" alt="Card image" style="width:100%; height: 160px;">
 						    <div class="card-body">
-						        <h4 class="card-title"><?php echo ucwords($row['item_name']); ?></h4>
+						        <h4 class="card-title" style="color:rgba(230,126,34);"><?php echo ucwords($row['item_name']); ?></h4>
 						        <p class="card-text pull-right"><?php echo ucfirst($row['item_type']); ?></p>
 						        <p class="card-text"><?php echo ucfirst($row['item_desc']); ?></p>
 						        <div class="card-text res-info">Restaurant Name - <?php echo ucwords($row['res_name']) ?></div>
 						        <a href="javascript:void(0);" class="btn btn-sm btn-info pull-right orderBtn">Order</a>
-						        <div class="card-text" style="margin-top: 8px;"><?php echo '₹'.$row['item_price']; ?></div>
+						        <div class="card-text" style="margin-top: 8px;color:rgba(230,126,34);"><?php echo '₹'.$row['item_price']; ?></div>
 						    </div>
 						</div>
         			</div>
@@ -73,6 +73,38 @@
 		</div>		
 	    		    
 	</div>
+<br>
+<footer class="font-small indigo" style="background-color:rgba(230, 126, 34);">
+<div class="container">
+    <div class="row text-center d-flex justify-content-center pt-5 mb-3">
+      <div class="col-md-3 mb-4">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="#!" style="color:white;"><b>About us</b></a>
+        </h6>
+      </div>
+      <div class="col-md-3 mb-4">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="" style="color:white;"><b>Feedback</b></a>
+        </h6>
+      </div>
+      <div class="col-md-3 mb-4">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="#!" style="color:white;"><b>Help</b></a>
+        </h6>
+      </div>
+      <div class="col-md-3 mb-4">
+        <h6 class="text-uppercase font-weight-bold">
+          <a href="#" style="color:white;"><b>Contact</b></a>
+        </h6>
+      </div>
+    </div>
+ <hr   style="margin: 0 8%;background-color:rgba(46,204,113);"><br>
+        <p style="line-height: 1.7rem;color:white;text-align:center;">You can put some text herethat you want to show in your footer part of the website</p>
+  <div class="footer-copyright text-center py-3" style="color:white;">© 2021 Copyright:
+    <span style="color:white"> Website</span>
+  </div>
+ 
+</footer>
 
 	<script type="text/javascript">
 		$('.orderBtn').on('click', function(){
