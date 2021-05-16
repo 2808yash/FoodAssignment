85e232f9cacaf8bb7e6c34aca525409bab406bc1
Yash Sharma <67325907+2808yash@users.noreply.github.com>
2021-05-16 10:25:01 +0530

    Update restaurant-login.php

--- restaurant-login.php
+++ restaurant-login.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>FoodShala</title>
+	<title>OrderNOW</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<!-- navbar -->
-	<nav class="navbar navbar-inverse navbar-fixed-top" style="font-size: 16px;">
+	<nav class="navbar navbar-inverse  navbar-fixed-top" style="font-size: 16px;background-color:rgba(230, 126, 34);border:0 solid white;">
 	  <div class="container">
 	    <div class="navbar-header">
 	    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -16,12 +16,12 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span> 
 		    </button>
-	      <a class="navbar-brand" href="index.php" style="font-size: 18px;">FoodSHALA for our partners</a>
+	      <a class="navbar-brand" href="index.php" style="font-size: 18px;color:white">OrderNOW for our partners</a>
 	    </div>
 	    <div class="collapse navbar-collapse" id="myNavbar">
 	      <ul class="nav navbar-nav navbar-right">
-	      	<li><a href="restaurant-signup.php">Join us today</a></li>
-	        <li class="active"><a href="restaurant-login.php"><span class="glyphicon glyphicon-log-in"></span> Sign in</a></li>
+	      	<li><a href="restaurant-signup.php" style="color:white;">Join us today</a></li>
+	        <li class="active"><a href="restaurant-login.php" style="color:white;"><span class="glyphicon glyphicon-log-in"></span> Sign in</a></li>
 	      </ul>
 	    </div>
 	  </div>
@@ -30,15 +30,15 @@
 	<div class="container col-md-6 col-md-offset-3">
 		<form action="files/res-authenticate.php" method="post">	
 		  <div class="form-group">
-		    <label for="email">Restaurant Email<b>*</b></label>
+		    <label for="email" style="color:rgba(230, 126, 34);">Restaurant Email<b>*</b></label>
 		    <input type="email" class="form-control" id="res_email" name="res_email" required/>
 		  </div>
 		  <div class="form-group">
-		    <label for="password">Password<b>*</b></label>
+		    <label for="password" style="color:rgba(230, 126, 34);">Password<b>*</b></label>
 		    <input type="password" class="form-control" id="res_password" name="res_password" required/>
 		  </div>
 		  <button type="submit" name="resSigninBtn" class="btn btn-success">Sign in</button>
-		   <span>Don't have account? <a class="text-danger" href="restaurant-signup.php">Sign up</a></span>
+		   <span>Don't have account? <a class="text-danger" href="user-signup.php">Sign up</a></span>
 		</form>
 	</div>	
 </body>
