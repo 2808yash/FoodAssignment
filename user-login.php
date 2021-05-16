1b1fa959813276226119c136e204508290c54029
Yash Sharma <67325907+2808yash@users.noreply.github.com>
2021-05-16 10:26:34 +0530

    Update user-login.php

--- user-login.php
+++ user-login.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>FoodShala</title>
+	<title>OrderNOW</title>
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
 <body>
 	<!-- navbar -->
-	<nav class="navbar navbar-inverse navbar-fixed-top" style="font-size: 16px;">
+	<nav class="navbar navbar-inverse  navbar-fixed-top" style="font-size: 16px;background-color:rgba(230, 126, 34);border:0 solid white;">
 	  <div class="container">
 	    <div class="navbar-header">
 	    	<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
@@ -16,13 +16,13 @@
 		        <span class="icon-bar"></span>
 		        <span class="icon-bar"></span> 
 		    </button>
-	      <a class="navbar-brand" href="index.php" style="font-size: 18px;">FoodSHALA</a>
+	      <a class="navbar-brand" href="index.php" style="font-size: 18px;color:white">OrderNOW</a>
 	    </div>
 	    <div class="collapse navbar-collapse" id="myNavbar">
 	      <ul class="nav navbar-nav navbar-right">
-	      	<li><a href="restaurant-signup.php">Join us today</a></li>
-	        <li><a href="user-signup.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-	        <li class="active"><a href="user-login.php"><span class="glyphicon glyphicon-log-in"></span> Sign in</a></li>
+	      	<li><a href="restaurant-signup.php" style="color:white;">Join us today</a></li>
+	        <li><a href="user-signup.php" style="color:white;"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+	        <li class="active"><a href="user-login.php" style="color:white;"><span class="glyphicon glyphicon-log-in"></span> Sign in</a></li>
 	      </ul>
 	    </div>
 	  </div>
@@ -31,16 +31,18 @@
 	<div class="container col-md-6 col-md-offset-3">
 		<form action="files/authenticate.php" method="post">	
 		  <div class="form-group">
-		    <label for="email">Email<b>*</b></label>
+		    <label for="email" style="color:rgba(230, 126, 34);">Email<b>*</b></label>
 		    <input type="email" class="form-control" id="email" name="email" required/>
 		  </div>
 		  <div class="form-group">
-		    <label for="password">Password<b>*</b></label>
+		    <label for="password" style="color:rgba(230, 126, 34);">Password<b>*</b></label>
 		    <input type="password" class="form-control" id="password" name="password" required/>
 		  </div>
 		  <button type="submit" name="userSigninBtn" class="btn btn-success">Sign in</button>
 		   <span>Don't have account? <a class="text-danger" href="user-signup.php">Sign up</a></span>
 		</form>
 	</div>	
+
+
 </body>
 </html>
